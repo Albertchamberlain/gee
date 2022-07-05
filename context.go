512152f87fa6d0d4d7b372bc25f698cf409b5e07
Amos <albert7king@gmail.com>
2022-07-05 23:55:24 +0800

    ✨ <add>:<easy to get the data>

---
+++ context.go
@@ -0,0 +1,73 @@
+// Context 随着每一个请求的出现而产生，请求的结束而销毁，
+// 和当前请求强相关的信息都应由 Context 承载。因此，设计 Context 结构，
+// 扩展性和复杂性留在了内部，而对外简化了接口。路由的处理函数，以及将要实现的中间件，
+// 参数都统一使用 Context 实例， Context 就像一次会话的百宝箱，可以找到任何东西。
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type H map[string]interface{} //这个H在gin中也是一样的功能
+
+type Context struct {
+	Writer     http.ResponseWriter
+	Req        *http.Request
+	Path       string
+	Method     string
+	StatusCode int
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500) //server error
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
