50c6edbb31fa414db72550dfe679726df3f9e49a
Amos <albert7king@gmail.com>
2022-07-18 10:34:58 +0800

    ♻️ <reaf>:<提取handler>

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ web framework in go
 gee框架的设计以及API均参考了gin。
 1. - [x] 使用New()创建 gee 的实例
 2. - [x] 使用 GET()方法添加路由
-3. - [x] 使用 Trie 树实现动态路由(dynamic route)解析。
+3. - [x] 使用 Trie 树实现动态路由解析。
 4. - [x] 支持两种模式:name和*filepath
 5. - [x] 以前缀来进行路由分组
 6. - [x] 中间件功能
 7. - [x] 错误恢复功能 
+8. - [ ] net/http -> fasthttp
--- main.go
+++ main.go
@@ -17,28 +17,33 @@ func onlyForV2() gee.HandlerFunc {
 		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
 	}
 }
+func helloHandler(c *gee.Context) {
+	c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+}
+
+func indexErrHandler(c *gee.Context) {
+	names := []string{"AMOS", "Jack", "Mike"}
+	c.String(http.StatusOK, names[100])
+}
+func v2Handler(c *gee.Context) {
+	// expect /hello/amos
+	c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+}
 
 func main() {
 	r := gee.New()
 	r.Use(gee.Logger()) // global midlleware
-	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-	})
+
+	r.GET("/", helloHandler)
 
 	v2 := r.Group("/v2")
 	v2.Use(onlyForV2()) // v2 group middleware
 	{
-		v2.GET("/hello/:name", func(c *gee.Context) {
-			// expect /hello/geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-		})
+		v2.GET("/hello/:name", v2Handler)
 	}
 
 	// index out of range for testing Recovery()
-	r.GET("/panic", func(c *gee.Context) {
-		names := []string{"AMOS", "Jack", "Mike"}
-		c.String(http.StatusOK, names[100])
-	})
+	r.GET("/panic", indexErrHandler)
 	r.Run(":9999")
 
 }
