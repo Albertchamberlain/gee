6ee92188dbca42d02b31ee1f901d72332f5a3538
Amos <albert7king@gmail.com>
2022-10-14 23:36:59 +0800

    📝

--- gee/recover.go
+++ gee/recover.go
@@ -26,6 +26,10 @@ func Recovery() HandlerFunc {
 }
 
 // print stack trace for debug
+// Recovery 的实现非常简单，使用 defer 挂载上错误恢复的函数，
+// 在这个函数中调用 *recover()*，捕获 panic，并且将堆栈信息打印在日志中，
+// 向用户返回 Internal Server Error。
+// trace() 函数，这个函数是用来获取触发 panic 的堆栈信息，
 // 在 trace() 中，调用了 runtime.Callers(3, pcs[:])，Callers 用来返回调用栈的程序计数器,
 // 第 0 个 Caller 是 Callers 本身，第 1 个是上一层 trace，第 2 个是再上一层的 defer func。
 // 因此，为了日志简洁一点，我们跳过了前 3 个 Caller。
